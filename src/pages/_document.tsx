495754b5bef3cf0f0f262c0b8493e4b72e18cd32
Renan Pereira <renanmol87@gmail.com>
2021-11-15 21:23:31 -0300

    feat(fonts): add web fonts in tailwind config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="http://fonts.gstatic.com"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <Main />
+        <NextScript />
+      </Html>
+    )
+  }
+}
